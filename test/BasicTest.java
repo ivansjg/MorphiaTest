2eade9b593d4e90a84d45291ed1e84f286667f28
Ivan San Jose Garcia <ivansjg@gmail.com>
2013-08-19 10:29:30 +0200

    Added two example of MongoDB raw querys

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -6,9 +6,19 @@ import play.test.*;
 import play.Logger;
 import play.libs.Codec;
 
+import com.mongodb.Mongo;
+import com.mongodb.DBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCursor;
+
+import com.google.code.morphia.Datastore;
+
 import org.bson.types.ObjectId;
 
 import play.modules.morphia.Model.MorphiaQuery;
+import play.modules.morphia.MorphiaPlugin;
 
 import models.*;
 
@@ -40,7 +50,7 @@ public class BasicTest extends UnitTest {
         Logger.info("name preflang(es): %s", user.preferredLanguage.name);
 
         MorphiaQuery q = User.q();
-        q.or(q.criteria("email").containsIgnoreCase("bOb"), 
+        q.or(q.criteria("email").containsIgnoreCase("bOb"),
              q.criteria("fullname").containsIgnoreCase("jeff"));
         List<User> lista = q.asList();
         assertEquals(lista.size(), 2);
@@ -54,6 +64,34 @@ public class BasicTest extends UnitTest {
             user.save();
         }
         */
+    }
+
+    @Test
+    public void lalala() {
+        // http://stackoverflow.com/questions/6871008/mongodb-embedded-object-creation-in-java-without-morphia
+        // http://docs.mongodb.org/manual/reference/operator/pull/#op._S_pull
+        Mongo mongo = MorphiaPlugin.ds().getMongo();
+        Datastore ds = MorphiaPlugin.ds();
+        DB db = ds.getDB();
+
+        ObjectId userId = (ObjectId) User.findByEmail("paul@gmail.com").getId();
+        DBObject query = new BasicDBObject("anotherOptionForFollowing", userId);
+        DBObject update = new BasicDBObject("$pull", new BasicDBObject("anotherOptionForFollowing", userId));
+
+        DBCollection col = db.getCollection("users");
+        col.update(query, update);
+
+        query = new BasicDBObject("email", new BasicDBObject("$ne", "paul@gmail.com"));
+        DBCursor cursor = col.find(query);
+
+        try {
+            while (cursor.hasNext()) {
+                Logger.info("-----------------------------------");
+                Logger.info("%s", cursor.next());
+            }
+        } finally {
+            cursor.close();
+        }
     }
 
 }
